8d01bc532c81d3b1085981dffd251562f128ab82
Puttu Pavan Kumar <101029891+Pavan1471@users.noreply.github.com>
2023-08-27 13:37:11 +0530

    modified:   bloodito/src/components/Sidebar.jsx 	modified:   bloodito/src/index.css 	modified:   bloodito/src/pages/Dashboard.jsx 	new file:   bloodito/src/pages/asserts/blood-drop.png

--- bloodito/src/components/Sidebar.jsx
+++ bloodito/src/components/Sidebar.jsx
@@ -76,7 +76,7 @@ const SidebarContent = ({ onClose, ...rest }) => {
   return (
     <Box
       transition="3s ease"
-      bg={useColorModeValue("red.100", "gray.900")}
+      bg={useColorModeValue("white", "gray.900")}
       borderRight="1px"
       borderRightColor={useColorModeValue("gray.200", "gray.700")}
       w={{ base: "full", md: 60 }}
@@ -144,7 +144,7 @@ const MobileNav = ({ onOpen, ...rest }) => {
       px={{ base: 4, md: 4 }}
       height="20"
       alignItems="center"
-      bg={useColorModeValue("red.100", "gray.900")}
+      bg={useColorModeValue("white", "gray.900")}
       borderBottomWidth="1px"
       borderBottomColor={useColorModeValue("gray.200", "gray.700")}
       justifyContent={{ base: "space-between", md: "flex-end" }}
--- bloodito/src/pages/Dashboard.jsx
+++ bloodito/src/pages/Dashboard.jsx
@@ -1,5 +1,41 @@
+import bloodlogo from "./asserts/blood-drop.png"
+import { FcApproval } from "react-icons/fc";
 const Dashboard = () => {
-  return <h1>Dashboard</h1>;
+  return(
+    <>
+    <div  className="donars-container-stats">
+    <div id="card-1" className="blooddonar-stats">
+      <img className="blood-logo" src={bloodlogo} alt="" />
+      <div>
+        <div className="donars-number">27</div>
+        <div className="donars-name">Donars</div>
+      </div>
+    </div>
+    <div className="blooddonar-stats">
+      <img className="blood-logo" src={bloodlogo} alt="" />
+      <div>
+        <div className="donars-number">24</div>
+        <div className="donars-name">Requests</div>
+      </div>
+    </div>
+    <div id="card-2" className="blooddonar-stats">
+      <img className="blood-logo" src={bloodlogo} alt="" />
+
+      <div>
+        <div className="donars-number">7</div>
+        <div className="donars-name">Approved</div>
+      </div>
+    </div>
+    <div className="blooddonar-stats">
+      <img className="blood-logo" src={bloodlogo} alt="" />
+      <div>
+        <div className="donars-number">0</div>
+        <div className="donars-name">Donars</div>
+      </div>
+    </div>
+    </div>
+    </>
+  )
 };
 
 export default Dashboard;
